f42e7d90d7707ac96c63e51d448bf98843311569
paul <paul0802@icloud.com>
2025-11-20 16:17:34 +0100

    added speed demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client"
+
+import {useEffect, useState} from "react";
+
 export default function Home() {
+    const [speed, setSpeed] = useState(0);
+
+    useEffect(() => {
+        const options = {
+            enableHighAccuracy: true
+        };
+
+        const speedObserver = navigator.geolocation.watchPosition(parsePosition,
+            null, options);
+    }, [])
+
+    const parsePosition = (position: GeolocationPosition) => {
+         setSpeed(Math.round((position?.coords?.speed || 0) * 3.6));
+    };
+
     return (
         <div className="text-2xl">
-            hi
+            hi(gh) speed {speed} km/h
         </div>
     )
 }
