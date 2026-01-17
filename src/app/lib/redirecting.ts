30f701e9955148743dd5f1e54f8239fd8d6ec71e
Vincent <vincent.berchi@outlook.com>
2026-01-17 18:52:51 +0100

    Redirecting added as a function

--- src/app/lib/redirecting.ts
+++ src/app/lib/redirecting.ts
@@ -3,23 +3,30 @@
 import {useScheduleLocationStore} from "@/store/userScheduleLocationStore"
 import {useLocationStore} from "@/store/userLocationStore"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 import distance from "@turf/distance";
 
 export function Redirecting() {
-  const items = useScheduleLocationStore((state) => state.items)
-  const location = useLocationStore()
-  const router = useRouter()
 
+    console.log("loading stroage")
+    const items = useScheduleLocationStore((state) => state.items)
+    const location = useLocationStore()
+    const router = useRouter()
 
+    console.log("loading time")
     const now = new Date()
     const currentMinutes = now.getHours() * 60 + now.getMinutes()
+
+
+    console.log("trying to find a match")
 
     const match = items.find((item) => {
         if(item.startTime && item.endTime)
         {
+
             const start = timeToMinutes(item.startTime)
             const end = timeToMinutes(item.endTime)
+            console.log("testing Time", currentMinutes, start, end)
+            
             return currentMinutes >= start && currentMinutes <= end
         } else if (item.lat && item.lon){
 
@@ -27,17 +34,18 @@ export function Redirecting() {
             const pointB: [number, number] = [location.lon? location.lon : 0.0, location.lat? location.lat : 0.0];
 
             const distInKm = distance(pointA, pointB, { units: "kilometers" });
-
+            console.log(pointA, pointB, distInKm)
             return distInKm <= 0.1; // 0.1 km = 100 meters
             //introduce distance
         }
-      
     })
 
     if (match) {
         let forward = `/monitor/${match.diva}?line=${match.line}&dir=${match.dir}`
         console.log(forward) 
         router.push(forward) 
+    } else{
+        console.log("No Match found!")
     }
   
 }
